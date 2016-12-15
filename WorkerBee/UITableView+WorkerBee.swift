8d0723df480a56f10bb3845a6ae100aba8eda6fe
nixzhu <zhuhongxu@gmail.com>
2016-12-15 11:10:35 +0800

    safe UITableView scrollToRow

---
+++ WorkerBee/UITableView+WorkerBee.swift
@@ -0,0 +1,18 @@
+//
+//  UITableView+WorkerBee.swift
+//  WorkerBee
+//
+//  Created by NIX on 2016/12/15.
+//  Copyright © 2016年 nixWork. All rights reserved.
+//
+
+import UIKit
+
+extension UITableView {
+
+    func workerbee_scrollToRow(at indexPath: IndexPath, at scrollPosition: UITableViewScrollPosition, animated: Bool) {
+        guard numberOfSections > indexPath.section else { return }
+        guard numberOfRows(inSection: indexPath.section) > indexPath.row else { return }
+        scrollToRow(at: indexPath, at: scrollPosition, animated: animated)
+    }
+}
