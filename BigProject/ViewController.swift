24ea2d2579fdde7ea38d6910cf6ba42278343a53
nixzhu <zhuhongxu@gmail.com>
2017-01-19 16:25:54 +0800

    add OperatingRoom

--- BigProject/ViewController.swift
+++ BigProject/ViewController.swift
@@ -33,9 +33,36 @@ class ViewController: UIViewController {
 
         let job = FreeTimeJob(target: self, selector: #selector(hardWork))
         job.commit()
+
+        operate { value in
+            print("or: value: \(value)")
+        }
     }
 
     func hardWork() {
         // TODO: hardWork
+    }
+
+    func operate(work: @escaping (Int) -> Void) {
+        let or = OperatingRoom()
+        var value = 0
+        or.prepare { finish in
+            DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                print("or: delay 2")
+                value = 2
+                finish()
+            }
+        }
+        or.prepare { finish in
+            DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
+                print("or: delay 5")
+                value = 5
+                finish()
+            }
+        }
+        or.ready {
+            print("or: go go go")
+            work(value)
+        }
     }
 }
---
+++ WorkerBee/OperatingRoom.swift
@@ -0,0 +1,31 @@
+//
+//  OperatingRoom.swift
+//  BigProject
+//
+//  Created by nixzhu on 2017/1/19.
+//  Copyright © 2017年 nixWork. All rights reserved.
+//
+
+import Foundation
+
+public class OperatingRoom {
+    private let dispatchGroup: DispatchGroup
+
+    public init() {
+        self.dispatchGroup = DispatchGroup()
+    }
+
+    public typealias Finish = () -> Void
+    public typealias Action = (@escaping Finish) -> Void
+
+    public func prepare(in action: Action) {
+        dispatchGroup.enter()
+        action {
+            self.dispatchGroup.leave()
+        }
+    }
+
+    public func ready(in queue: DispatchQueue? = nil, for work: @escaping () -> Void) {
+        dispatchGroup.notify(queue: queue ?? .main, execute: work)
+    }
+}
